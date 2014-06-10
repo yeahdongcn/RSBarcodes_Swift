e50944d4117ce432b2e1c36d022347754bb30ba1
R0CKSTAR <yeahdongcn@gmail.com>
2014-06-10 12:19:22 +0800

    Init commit

---
+++ RSBarcodesSample/RSBarcodesSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/10/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
