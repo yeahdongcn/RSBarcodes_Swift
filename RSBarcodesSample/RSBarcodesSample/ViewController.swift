6736db1c10bd3eac15789ae5ec020f5b02f8244b
R0CKSTAR <yeahdongcn@gmail.com>
2014-06-12 12:21:27 +0800

    啥也没干啊

---
+++ RSBarcodesSample/RSBarcodesSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/10/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
