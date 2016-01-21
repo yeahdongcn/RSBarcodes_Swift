835ecdbc9e65dfb0f4406451115223a1e9f8fd50
Jarvie8176 <kongjw1993@gmail.com>
2016-01-21 13:09:36 -0500

    Example update
    What it does:
    
    * scan for barcodes
    * once a barcode is scanned, show the details of the barcode (decoded
    value and a barcode generated based on the value decoded) in the next
    view
    * when user presses back button, scan for another barcode
    
    Know issues:
    
    recenter doesn’t work - need to put the barcode in the center of screen
    even after user tapped on the screen.

--- RSBarcodesSample/RSBarcodesSample/BarcodeReaderViewController.swift
+++ RSBarcodesSample/RSBarcodesSample/BarcodeReaderViewController.swift
@@ -3,6 +3,9 @@
 //  RSBarcodesSample
 //
 //  Created by R0CKSTAR on 6/10/14.
+//
+//  Updated by Jarvie8176 on 01/21/2016
+//
 //  Copyright (c) 2014 P.D.Q. All rights reserved.
 //
 
@@ -23,6 +26,9 @@ class BarcodeReaderViewController: RSCodeReaderViewController {
         print(isTorchOn)
     }
     
+    var barcode: String = ""
+    var dispatched: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,14 +40,6 @@ class BarcodeReaderViewController: RSCodeReaderViewController {
             print(point)
         }
         
-        self.barcodesHandler = { barcodes in
-            for barcode in barcodes {
-                print("Barcode found: type=" + barcode.type + " value=" + barcode.stringValue)
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    // MARK: NOTE: Perform UI related actions here.
-                })
-            }
-        }
         
         let types = NSMutableArray(array: self.output.availableMetadataObjectTypes)
         types.removeObject(AVMetadataObjectTypeQRCode)
@@ -55,15 +53,39 @@ class BarcodeReaderViewController: RSCodeReaderViewController {
         if !self.hasTorch() {
             self.toggle.enabled = false
         }
+        
+        self.barcodesHandler = { barcodes in
+            if !self.dispatched { // triggers for only once
+                self.dispatched = true
+                for barcode in barcodes {
+                    self.barcode = barcode.stringValue
+                    print("Barcode found: type=" + barcode.type + " value=" + barcode.stringValue)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.performSegueWithIdentifier("nextView", sender: self)
+                        
+                        // MARK: NOTE: Perform UI related actions here.
+                    })
+                }
+            }
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
+        self.dispatched = false // reset the flag so user can do another scan
+        
         super.viewWillAppear(animated)
         
         self.navigationController?.navigationBarHidden = true
+        
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         self.navigationController?.navigationBarHidden = false
+        
+        if segue.identifier == "nextView" {
+            let destinationVC = segue.destinationViewController as! BarcodeDisplayViewController
+            destinationVC.contents = self.barcode
+        }
     }
 }
