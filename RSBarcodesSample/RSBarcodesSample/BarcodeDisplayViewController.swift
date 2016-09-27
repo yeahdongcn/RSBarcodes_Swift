aa0be8b53946ba2a1044cd9c5f590f82708e9578
R0CKSTAR <yeahdongcn@gmail.com>
2016-09-27 15:44:14 +0800

    Update sb and sample

--- RSBarcodesSample/RSBarcodesSample/BarcodeDisplayViewController.swift
+++ RSBarcodesSample/RSBarcodesSample/BarcodeDisplayViewController.swift
@@ -22,16 +22,18 @@ class BarcodeDisplayViewController: UIViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = contents
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
         let gen = RSUnifiedCodeGenerator.shared
         gen.fillColor = UIColor.white
         gen.strokeColor = UIColor.black
         print ("generating image with barcode: " + contents)
-        let image: UIImage? = gen.generateCode(contents, machineReadableCodeObjectType: AVMetadataObjectTypeQRCode)
-        
-        if (image != nil) {
+        if let image = gen.generateCode(contents, machineReadableCodeObjectType: AVMetadataObjectTypeQRCode) {
             self.imageDisplayed.layer.borderWidth = 1
-            self.imageDisplayed.image = RSAbstractCodeGenerator.resizeImage(image!, targetSize: self.imageDisplayed.bounds.size, contentMode: UIViewContentMode.bottomRight)
+            self.imageDisplayed.image = RSAbstractCodeGenerator.resizeImage(image, targetSize: self.imageDisplayed.bounds.size, contentMode: UIViewContentMode.bottomRight)
         }
     }
 }
