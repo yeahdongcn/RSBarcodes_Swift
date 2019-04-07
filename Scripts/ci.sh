4f5f30fdabba80dd316bbad61627a2faf1843551
YanMeow <liyan_helloworld@163.com>
2019-04-07 12:44:28 +0800

    Update travis CI config

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -1,5 +1,5 @@
 set -e
 
-xcodebuild -project RSBarcodes.xcodeproj -scheme "RSBarcodes" -destination "platform=iOS Simulator,name=iPhone 6" build
+xcodebuild -project RSBarcodes.xcodeproj -scheme "RSBarcodes" -destination "platform=iOS Simulator,name=iPhone 6" clean build
 
-xcodebuild -project RSBarcodesSample/RSBarcodesSample.xcodeproj -scheme "RSBarcodesSample" -destination "platform=iOS Simulator,name=iPhone 6" test
+xcodebuild -project RSBarcodesSample/RSBarcodesSample.xcodeproj -scheme "RSBarcodesSample" -destination "platform=iOS Simulator,name=iPhone 6" clean test
