fc9a8d33715277c70a9be92f2d7ba4d53b6dda75
Alexey Korolev <alphatroya@gmail.com>
2019-10-11 10:19:24 +0300

    Add Package.swift file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "RSBarcodes_Swift",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(name: "RSBarcodes_Swift", targets: ["RSBarcodes_Swift"]),
+    ],
+    targets: [
+        .target(
+            name: "RSBarcodes_Swift",
+            path: "Source"
+        ),
+    ]
+)
